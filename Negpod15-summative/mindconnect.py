877375113b47d520fc4ba5e4a17f8c41d09a2a54
julianacholder <j.holder@alustudent.com>
2023-08-05 11:11:58 -0700

    first commit

---
+++ Negpod15-summative/mindconnect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+username = input("Enter your username to sign in: ")
+print("Welcome to MindConnect, {}!".format(username))
+
+disclaimer = """
+Disclaimer: 
+
+The MindConnect App and its integrated support chatbot provide information and tools to promote mental health wellbeing. However, they are not a substitute for professional medical advice or treatment. Users should consult qualified healthcare professionals for personalized guidance. Users interact with the app and chatbot at their own risk and are responsible for their decisions. In case of an emergency, please seek immediate assistance from a medical professional or call your local emergency number.
+"""
+
+print(disclaimer)
+
