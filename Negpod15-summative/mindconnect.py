4cafaaffdcd1de7fe0b38b8549eee04c5c9152e5
root <m.george@alustudent.com>
2023-08-06 11:47:03 +0000

    1

--- Negpod15-summative/mindconnect.py
+++ Negpod15-summative/mindconnect.py
@@ -20,4 +20,10 @@ import random
 '''Define a class MentalHealthApp'''
 class MentalHealthApp:
      '''presend a  menu of the app'''
-
+	def __init__(self):
+		self.resources = ["Resource 1", "Resource 2", "Resource 3"]
+		self.self_assessment_tools = [
+			"Assessment Tool 1",
+			"Assessment Tool 2",
+			"Assessment Tool 3",
+		]
