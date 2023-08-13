27f4eaae4d3a5107d5cb69baa3d96112fe392699
Mugisha Kenny Kevin <127345283+KennyKvn001@users.noreply.github.com>
2023-08-13 22:35:40 +0200

    MC-app

--- Negpod15-summative/mindconnect_app.py
+++ Negpod15-summative/mindconnect_app.py
@@ -2,6 +2,21 @@
 
 import sys
 import random
+import time
+
+
+username = input("Enter your username to sign in: ")
+print("Welcome to MindConnect, {}!".format(username))
+time.sleep(1)
+
+disclaimer = """
+Disclaimer: 
+
+The MindConnect App and its integrated support chatbot provide information and tools to promote mental health wellbeing. However, they are not a substitute for professional medical advice or treatment. Users should consult qualified healthcare professionals for personalized guidance. Users interact with the app and chatbot at their own risk and are responsible for their decisions. In case of an emergency, please seek immediate assistance from a medical professional or call your local emergency number.
+"""
+
+print(disclaimer)
+time.sleep(1)
 
 '''Define a class MentalHealthApp'''
 
